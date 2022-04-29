f60f46c940083942f5030b52555c2349ff72489a
Achalwan <101328264+Achalwan@users.noreply.github.com>
2022-04-29 16:28:55 +0530

    Add files via upload

---
+++ Prime.py
@@ -0,0 +1,11 @@
+num=20
+if num > 1:
+    for i in range (2,num//2):
+        if (num%i)==0:
+            print (num,"is a prime number")
+            break
+        else:
+            print(num,"isnot a prime number")
+            
+20 is a prime number
+>>> 
